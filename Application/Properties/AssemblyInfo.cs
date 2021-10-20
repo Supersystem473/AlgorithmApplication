eb42506a11373659c844f563f471f3477cb7f906
Supersystem <Supersystem473@github.com>
2021-10-20 12:37:32 -0400

    Part 3

--- Application/Properties/AssemblyInfo.cs
+++ Application/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("bcdad502-55c6-4ab9-ae17-ec91034b1dee")]
+[assembly: Guid("3342c86f-5eeb-422d-a1ac-bd2bfe2f2634")]
 
 // Version information for an assembly consists of the following four values:
 //
